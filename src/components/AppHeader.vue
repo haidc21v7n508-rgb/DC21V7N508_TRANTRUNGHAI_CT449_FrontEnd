18348cfa864d3af1322a1440184eee62ff4684c7
Tran Trung Hai <haidc21v7n508@vlvh.ctu.edu.vn>
2025-10-02 21:17:35 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav class="navbar navbar-expand navbar-dark bg-dark">
+    <router-link to="/" class="navbar-brand">
+      Ứng dụng Quản lý danh bạ
+    </router-link>
+    <div class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <router-link :to="{ name: 'contactbook' }" class="nav-link">
+          <i class="fas fa-address-book"></i> Danh bạ
+        </router-link>
+      </li>
+    </div>
+  </nav>
+</template>
